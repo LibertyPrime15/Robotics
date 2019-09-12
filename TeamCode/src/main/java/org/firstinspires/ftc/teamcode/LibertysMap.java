a43ab0b5479bf5d1c402b018b76b008a24054f6a
nihufs20 <nihufs20@nsd.newberg.k12.or.us>
2019-09-12 09:52:26 -0700

    This has the hardware map in it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LibertysMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LibertysMap.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class HardwareMap
+public class LibertysMap
 {
     /* Public OpMode members. */
-    public DcMotor  front_left   = null;
-    public DcMotor  front_right  = null;
+    public DcMotor  front_right   = null;
+    public DcMotor  front_left    = null;
     public DcMotor  back_left     = null;
     public DcMotor  back_right    = null;
 
@@ -15,7 +16,7 @@ public class HardwareMap
     private ElapsedTime period  = new ElapsedTime();
 
     /* Constructor */
-    public HardwareMap(){
+    public LibertysMap(){
 
     }
 
@@ -24,11 +25,15 @@ public class HardwareMap
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-        front_left  = hwMap.get(DcMotor.class, "front_left");
-        front_right = hwMap.get(DcMotor.class, "front_right");
-        back_right   = hwMap.get(DcMotor.class, "back_left");
-        back_left  = hwMap.get(DcMotor.class, "back_right");
+        front_right  = hwMap.get(DcMotor.class, "front_right");
+        front_left = hwMap.get(DcMotor.class, "front_left");
+        back_right   = hwMap.get(DcMotor.class, "back_right");
+        back_left  = hwMap.get(DcMotor.class, "back_left");
 
+        front_right.setPower(0);
+        front_left.setPower(0);
+        back_right.setPower(0);
+        back_left.setPower(0);
 
         front_right.setDirection(DcMotor.Direction.FORWARD);
         front_left.setDirection(DcMotor.Direction.FORWARD);
@@ -37,17 +42,12 @@ public class HardwareMap
 
         front_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         front_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        back_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         back_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         front_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         front_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         back_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         back_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        front_right.setPower(0);
-        front_left.setPower(0);
-        back_right.setPower(0);
-        back_left.setPower(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearBoi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearBoi.java
@@ -1,86 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="Colee's TeleOp", group = "Main")
 public class LinearBoi extends LinearOpMode
 {
-    DcMotor front_right;
-    DcMotor front_left;
-    DcMotor back_right;
-    DcMotor back_left;
-
-    //----------------------------------------------------------------------------------------------
-    private void FowardPosBackNeg(double pow)
+    LibertysMap robot = new LibertysMap();
+//----------------------------------------------------------------------------------------------
+    @Override
+    public void runOpMode()
     {
-        front_right.setPower(pow);
-        front_left.setPower(pow);
-        back_left.setPower(pow);
-        back_right.setPower(pow);
-    }
-    //----------------------------------------------------------------------------------------------
-    private void Halt(){
-        front_right.setPower(0);
-        front_left.setPower(0);
-        back_left.setPower(0);
-        back_right.setPower(0);
-    }
-    //----------------------------------------------------------------------------------------------
-    public void runOpMode(){
-
         robot.init(hardwareMap);
-
         telemetry.addData("Status", "Let's Go Boisssss");
         telemetry.update();
 
-//        //------------------------------------------------------------------------------------------
-//        front_right = hardwareMap.get(DcMotor.class, "front_right");
-//        back_left = hardwareMap.get(DcMotor.class, "front_left");
-//        front_left = hardwareMap.get(DcMotor.class, "back_right");
-//        back_right = hardwareMap.get(DcMotor.class, "back_left");
-//        //------------------------------------------------------------------------------------------
-//        front_right.setDirection(DcMotorSimple.Direction.FORWARD);
-//        front_left.setDirection(DcMotorSimple.Direction.FORWARD);
-//        back_left.setDirection(DcMotorSimple.Direction.REVERSE);
-//        back_right.setDirection(DcMotorSimple.Direction.FORWARD);
-//
-//        //------------------------------------------------------------------------------------------
-//        front_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        front_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        back_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        back_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//
-//        front_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        front_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        back_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        back_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        //------------------------------------------------------------------------------------------
         waitForStart();
 
         while(opModeIsActive()){
-
-            back_left.setPower(0);
-            front_right.setPower(0);
-            back_right.setPower(0);
-            front_left.setPower(0);
-
             if(gamepad1.right_stick_y!=0)
             {
-                back_left.setPower(gamepad1.right_stick_y);
-                front_right.setPower(gamepad1.right_stick_y);
+                robot.back_left.setPower(gamepad1.right_stick_y);
+                robot.front_right.setPower(gamepad1.right_stick_y);
             }
 
             if(gamepad1.left_stick_y!=0)
             {
-                back_right.setPower(gamepad1.left_stick_y);
-                front_left.setPower(gamepad1.left_stick_y);
+                robot.back_right.setPower(gamepad1.left_stick_y);
+                robot.front_left.setPower(gamepad1.left_stick_y);
             }
         }
     }
