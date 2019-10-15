fa5ad18e18a29749ae820d40e08caae7a98f6ca4
nihufs20 <nihufs20@nsd.newberg.k12.or.us>
2019-10-15 14:02:15 -0700

    I'm adding in our new skystone project folder with some updated teleOp and Autonoumous stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevAuto.java
@@ -0,0 +1,168 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@Autonomous(name ="Rev Auto", group = "Concept")
+//@Disabled
+public class RevAuto extends LinearOpMode
+{
+    RevMap robot = new RevMap();
+    Orientation angles;
+    BNO055IMU imu;
+
+    float currHeading = 0;
+//--------------------------------------------------------------------------------------------------
+//----------------------------------------//
+//----------------------------------------//
+//---These are all of my Called Methods---//
+//----------------------------------------//
+//----------------------------------------//
+//--------------------------------------------------------------------------------------------------
+    private void turnAngle(double angle, double thing)
+    {
+        if(angle > 0)
+        {
+            while(angle > currHeading && (!(isStopRequested())))
+            {
+                telemetry.addLine().addData("Heading", currHeading);
+                telemetry.update();
+                angles = this.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                this.imu.getPosition();
+                currHeading = angles.firstAngle;
+                robot.LTurn(thing);
+            }
+            imuInit();
+        }
+
+        else if(angle < 0)
+        {
+            while(angle < currHeading && (!(isStopRequested())))
+            {
+                telemetry.addLine().addData("---Heading", currHeading);
+                telemetry.update();
+                angles = this.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                this.imu.getPosition();
+                currHeading = angles.firstAngle;
+                robot.RTurn(thing);
+            }
+            imuInit();
+        }
+    }
+//--------------------------------------------------------------------------------------------------
+    public void moveDistance(double length)
+    {
+        double totDistInSteps = (((length / 11.97) * 1120) * -1);
+
+        //IF THE NUMBER IS A POSITIVE NUMBER WE GO FORWARD!
+        if (totDistInSteps < robot.front_right.getCurrentPosition())
+        {
+            while(totDistInSteps <= robot.front_right.getCurrentPosition() && (!(isStopRequested())))
+            {
+                telemetry.addData("Current Value",robot.front_right.getCurrentPosition());
+                telemetry.addData("totDistInSteps",totDistInSteps);
+                telemetry.update();
+                robot.Forward(.1);
+            }
+        }
+        //IF THE NUMBER IS A NEGATIVE NUMBER WE GO BACKWARD!
+        else if (totDistInSteps > robot.front_right.getCurrentPosition())
+        {
+            while (totDistInSteps >= robot.front_right.getCurrentPosition() && (!(isStopRequested())))
+            {
+                telemetry.addData("---Current Value",robot.front_right.getCurrentPosition());
+                telemetry.addData("---totDistInSteps",totDistInSteps);
+                telemetry.update();
+                robot.Backward(.1);
+            }
+        }
+        robot.Halt();
+    }
+//--------------------------------------------------------------------------------------------------
+    public void imuInit()
+    {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        robot.init(hardwareMap);
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+    }
+//--------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------
+    public void runOpMode()
+    {
+        imuInit();
+        waitForStart();
+
+        if (opModeIsActive() && (!(isStopRequested())))
+        {
+            waitForStart();
+            robot.runtime.reset();
+//--------------------------------------------------------------------------------------------------
+            while (opModeIsActive() && (!(isStopRequested())))
+            {
+                robot.runtime.reset();
+//---------------------------------------\/ \/ \/ \/ Code Goes there
+                turnAngle(35, .5);
+
+                moveDistance(10);
+
+                turnAngle(-45 ,.5);
+
+                moveDistance(-25);
+//---------------------------------------/\ /\ /\ /\ Code Goes There
+                stop();
+            }
+        }
+    }
+}
+//--------------------------------------------------------------------------------------------------
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VufoMap.java
@@ -0,0 +1,137 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//@Disabled
+public class VufoMap
+{
+    /* Public OpMode members */
+    public DcMotor  front_right   = null;
+    public DcMotor  front_left    = null;
+    public DcMotor  back_left     = null;
+    public DcMotor  back_right    = null;
+
+    public DcMotor  lift          = null;
+    public DcMotor  arm           = null;
+
+    public Servo claw1 = null;
+    public Servo claw2 = null;
+
+    public ElapsedTime runtime = new ElapsedTime();
+//--------------------------------------------------------------------------------------------------
+    HardwareMap hwMap  =  null;
+    public VufoMap(){}
+
+    public void init(HardwareMap ahwMap)
+    {
+        hwMap  = ahwMap;
+
+        front_right = hwMap.get(DcMotor.class, "front_right");
+        front_left  = hwMap.get(DcMotor.class, "front_left");
+        back_right  = hwMap.get(DcMotor.class, "back_right");
+        back_left   = hwMap.get(DcMotor.class, "back_left");
+
+        lift  = hwMap.get(DcMotor.class, "lift");
+        arm   = hwMap.get(DcMotor.class, "arm");
+
+        claw1  = hwMap.get(Servo.class, "claw1");
+        claw2  = hwMap.get(Servo.class, "claw2");
+
+        front_right.setPower(0);
+        front_left.setPower(0);
+        back_right.setPower(0);
+        back_left.setPower(0);
+
+        lift.setPower(0);
+        arm.setPower(0);
+
+        claw1.setPosition(0);
+        claw2.setPosition(0);
+
+        front_right.setDirection(DcMotor.Direction.REVERSE);
+        front_left.setDirection(DcMotor.Direction.FORWARD);
+        back_right.setDirection(DcMotor.Direction.REVERSE);
+        back_left.setDirection(DcMotor.Direction.FORWARD);
+
+        lift.setDirection(DcMotor.Direction.FORWARD);
+        arm.setDirection(DcMotor.Direction.REVERSE);
+
+        front_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        front_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        front_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        front_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+//----------------------------------------//
+    //Stop
+    public void Halt()
+    {
+        front_right.setPower(0);
+        front_left.setPower(0);
+        back_right.setPower(0);
+        back_left.setPower(0);
+    }
+//----------------------------------------//
+    //Forward
+    public void Forward(double power)
+    {
+        front_right.setPower(-power);
+        front_left.setPower(-power);
+        back_right.setPower(-power);
+        back_left.setPower(-power);
+    }
+//----------------------------------------//
+    //Backward
+    public void Backward(double power)
+    {
+        front_right.setPower(power);
+        front_left.setPower(power);
+        back_right.setPower(power);
+        back_left.setPower(power);
+    }
+//----------------------------------------//
+    //LTurn
+    public void LTurn(double power)
+    {
+        front_right.setPower(-power);
+        front_left.setPower(power);
+        back_right.setPower(-power);
+        back_left.setPower(power);
+    }
+//----------------------------------------//
+    //RTurn
+    public void RTurn(double power)
+    {
+        front_right.setPower(power);
+        front_left.setPower(-power);
+        back_right.setPower(power);
+        back_left.setPower(-power);
+    }
+//----------------------------------------//
+    //Reset all of the encoder values
+    public void resetEncoder()
+    {
+        front_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        front_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        front_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        front_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+//----------------------------------------//
+}
